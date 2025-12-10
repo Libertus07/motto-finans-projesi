9d1858d1fa52c20f1877b93346d82545ccd5f914
Libertus07 <76736527+Libertus07@users.noreply.github.com>
2025-12-10 06:29:02 +0300

    Vite build optimizasyonu yapıldı

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// Vercel'da kaynak dosyalarının (JS/CSS) doğru yüklenmesi için base ayarı eklendi.
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
+  build: {
+    chunkSizeWarningLimit: 1000, // Uyarı limitini 500kb'den 1000kb'ye çıkarır
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Node_modules içindeki kütüphaneleri ayrı paketle
+          if (id.includes('node_modules')) {
+            // Firebase'i ayrı bir dosyaya koy
+            if (id.includes('firebase')) {
+              return 'firebase';
+            }
+            // Grafik kütüphanesini ayrı dosyaya koy
+            if (id.includes('recharts')) {
+              return 'recharts';
+            }
+            // İkonları ayrı dosyaya koy
+            if (id.includes('lucide-react')) {
+              return 'lucide';
+            }
+            
+            // Geri kalan her şeyi vendor.js içine koy
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
+})
