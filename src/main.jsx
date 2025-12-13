b34b63198f241624c8e1b69d70be837b44b19412
Libertus07 <76736527+Libertus07@users.noreply.github.com>
2025-12-13 10:51:57 +0300

    Vercel Speed Insights ve Analytics güncellendii

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import './index.css'
 import App from './App.jsx'
 
 // 👇 Vercel Performans ve Analiz Araçları
-import { SpeedInsights } from "@vercel/speed-insights/react"
-import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
